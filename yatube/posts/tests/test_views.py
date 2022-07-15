f7307b5e18fdee14ae89bd210738553d02d9775c
Rustam <Rustamaaaa@bk.ru>
2022-07-15 19:23:26 +0300

    refactor: вынести тести в отдельные классы

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -6,7 +6,7 @@ from django.core.cache import cache
 from posts.models import Post, Group, User, Follow
 
 
-class TestingViewsInPosts(TestCase):
+class TestingViews(TestCase):
     @classmethod
     def setUpClass(cls):
         """ Создать таблицы. """
@@ -65,228 +65,26 @@ class TestingViewsInPosts(TestCase):
             'post_create': reverse('posts:post_create')
         }
 
-        cls.urls_pagination = (
-            cls.urls['index'],
-            cls.urls['group_posts'],
-            cls.urls['profile'],
-            cls.urls['follow_index'],
-            cls.urls['follow_author']
-        )
-
-        cls.urls_and_templates_public = {
-            cls.urls['index']: 'posts/index.html',
-            cls.urls['group_posts']: 'posts/group_list.html',
-            cls.urls['post_detail']: 'posts/post_detail.html',
-            cls.urls['profile']: 'posts/profile.html'
-        }
-
-        cls.urls_and_templates_private = {
-            cls.urls['post_create']: 'posts/create_post.html',
-            cls.urls['follow_index']: 'posts/follow.html',
-            cls.urls['post_edit']: 'posts/create_post.html',
-            cls.urls['follow_author']: 'posts/follow.html'
-        }
-
     def setUp(self):
         """ Создать авторизированного пользователя. """
         self.authorized_client_other = Client()
 
         # Авторизировать пользователя, не автора поста.
         self.authorized_client_other.force_login(
-            TestingViewsInPosts.user_other
+            TestingViews.user_other
         )
 
         self.authorized_client = Client()
 
         # Авторизировать пользователя автора поста.
-        self.authorized_client.force_login(TestingViewsInPosts.user)
-
-    def test_pages_uses_correct_templates_for_public_urls(self):
-        """ Проверка соответствие шаблонов для публичных адресов. """
-        for url, template in self.urls_and_templates_public.items():
-            response = self.authorized_client.get(url)
-
-            with self.subTest(url=url):
-                self.assertTemplateUsed(response, template)
-
-    def test_pages_uses_correct_templates_for_private_urls(self):
-        """ Проверка соответствие шаблонов для приватных адресов. """
-        for url, template in self.urls_and_templates_private.items():
-            response = self.authorized_client.get(url)
-
-            with self.subTest(url=url):
-                self.assertTemplateUsed(response, template)
-
-    def test_correct_form_when_creating_posts(self):
-        """ Тестируем форму при создания поста. """
-        response = self.authorized_client.get(self.urls['post_create'])
-
-        form_fields = {
-            'text': forms.fields.CharField,
-            'group': forms.fields.ChoiceField
-        }
-
-        for value, expected in form_fields.items():
-            with self.subTest(value=value):
-                form_field = response.context['form'].fields[value]
-
-                self.assertIsInstance(form_field, expected)
-
-    def test_correct_form_when_editing_posts(self):
-        """ Тестируем форму при редактирования поста. """
-        response = self.authorized_client.get(self.urls['post_edit'])
-
-        form_fields = {
-            'text': forms.fields.CharField,
-            'group': forms.fields.ChoiceField
-        }
-
-        for value, expected in form_fields.items():
-            with self.subTest(value=value):
-                form_field = response.context['form'].fields[value]
-
-                self.assertIsInstance(form_field, expected)
-
-    def test_correct_show_context_posts(self):
-        """ Проверка корректности контекста post. """
-        urls = (
-            self.urls['post_detail'],
-            self.urls['post_edit']
-        )
-
-        for url in urls:
-            response = self.authorized_client.get(url)
-
-            with self.subTest(url=url):
-                self.assertEqual(
-                    response.context.get('post'),
-                    TestingViewsInPosts.post
-                )
-
-    def test_correct_show_context_is_edits(self):
-        """ Проверка корректности контекста is_edit. """
-
-        urls_and_values = {
-            self.urls['post_edit']: True,
-            self.urls['post_create']: False
-        }
-
-        for url, value in urls_and_values.items():
-            response = self.authorized_client.get(url)
-
-            with self.subTest(url=url):
-                self.assertEqual(
-                    response.context.get('is_edit'),
-                    value
-                )
-
-    def test_correct_show_context_group(self):
-        """ Проверка корректности контекста group, на странице групп. """
-        response_group = self.authorized_client.get(self.urls['group_posts'])
-
-        self.assertEqual(
-            TestingViewsInPosts.group,
-            response_group.context.get('group')
-        )
-
-    def test_correct_show_context_is_author_if_user_authorized(self):
-        """ 
-        Проверка корректности контекста author,
-        если пользователь автор поста.
-        """
-
-        # Пользователь автор поста
-        response_author = self.authorized_client.get(self.urls['post_detail'])
-
-        self.assertEqual(
-            response_author.context.get('author'),
-            TestingViewsInPosts.user
-        )
-
-    def test_correct_show_context_is_author_if_user_not_authorized(self):
-        """
-        Проверка корректности контекста author,
-        если пользователь не автор поста.
-        """
-
-        # Пользователь не автор поста
-        response_other = self.authorized_client_other.get(
-            self.urls['post_detail']
-        )
-
-        self.assertNotEqual(
-            response_other.context.get('author'),
-            TestingViewsInPosts.user
-        )
-
-    def test_correct_show_context_posts_count(self):
-        """ Проверка корректности контекста количество постов. """
-        urls = (
-            self.urls['post_detail'],
-            self.urls['profile']
-        )
-
-        for url in urls:
-            response = self.authorized_client.get(url)
-
-            with self.subTest(url=url):
-                self.assertEqual(
-                    response.context.get('post_count'),
-                    Post.objects.count()
-                )
-
-    def test_correct_show_context_post_authors(self):
-        """ Проверка корректности контекста в авторах поста. """
-        urls = (
-            self.urls['profile'],
-        )
-
-        for url in urls:
-            response = self.authorized_client.get(url)
-
-            with self.subTest(url=url):
-                self.assertEqual(
-                    response.context.get('author'),
-                    TestingViewsInPosts.post.author
-                )
-
-    def test_correct_show_context_groups(self):
-        """ Проверка корректности контекста в группах. """
-        urls = (
-            self.urls['group_posts'],
-        )
-
-        for url in urls:
-            response = self.authorized_client.get(url)
-
-            with self.subTest(url=url):
-                self.assertEqual(
-                    response.context.get('group'),
-                    TestingViewsInPosts.post.group
-                )
-
-    def test_correct_show_context_authors(self):
-        """ Проверка корректности контекста авторов. """
-        urls = {
-            self.urls['follow_index'],
-            self.urls['follow_author']
-        }
-
-        for url in urls:
-            response = self.authorized_client.get(url)
-
-            with self.subTest(url=url):
-                self.assertEqual(
-                    response.context.get('authors')[0],
-                    TestingViewsInPosts.follow.author
-                )
+        self.authorized_client.force_login(TestingViews.user)
 
     def test_check_display_posts_on_pages(self):
         """ Проверить наличие постов на страницах где они отображаются. """
         new_post = Post.objects.create(
             text='Описание поста',
-            author=TestingViewsInPosts.user,
-            group=TestingViewsInPosts.group
+            author=TestingViews.user,
+            group=TestingViews.group
         )
 
         pages = {
@@ -321,23 +119,218 @@ class TestingViewsInPosts(TestCase):
             response.context.get('page_obj').object_list
         )
 
+
+class TestingViewsCheckTemplate(TestingViews):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.urls_and_templates_public = {
+            cls.urls['index']: 'posts/index.html',
+            cls.urls['group_posts']: 'posts/group_list.html',
+            cls.urls['post_detail']: 'posts/post_detail.html',
+            cls.urls['profile']: 'posts/profile.html'
+        }
+
+        cls.urls_and_templates_private = {
+            cls.urls['post_create']: 'posts/create_post.html',
+            cls.urls['follow_index']: 'posts/follow.html',
+            cls.urls['post_edit']: 'posts/create_post.html',
+            cls.urls['follow_author']: 'posts/follow.html'
+        }
+
+    def test_pages_uses_correct_templates_for_public_urls(self):
+        """ Проверка соответствие шаблонов для публичных адресов. """
+        for url, template in self.urls_and_templates_public.items():
+            response = self.authorized_client.get(url)
+
+            with self.subTest(url=url):
+                self.assertTemplateUsed(response, template)
+
+    def test_pages_uses_correct_templates_for_private_urls(self):
+        """ Проверка соответствие шаблонов для приватных адресов. """
+        for url, template in self.urls_and_templates_private.items():
+            response = self.authorized_client.get(url)
+
+            with self.subTest(url=url):
+                self.assertTemplateUsed(response, template)
+
+
+class TestingViewsCheckContext(TestingViews):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.urls_for_check_post_context = (
+            cls.urls['post_detail'],
+            cls.urls['post_edit']
+        )
+
+        cls.urls_and_values_for_check_is_edit_context = {
+            cls.urls['post_edit']: True,
+            cls.urls['post_create']: False
+        }
+
+        cls.urls_for_check_post_count_context = (
+            cls.urls['post_detail'],
+            cls.urls['profile']
+        )
+
+        cls.urls_for_check_author_context = (
+            cls.urls['profile'],
+        )
+
+        cls.urls_for_check_group_context = (
+            cls.urls['group_posts'],
+        )
+
+        cls.urls_for_check_authors_context = (
+            cls.urls['follow_index'],
+            cls.urls['follow_author']
+        )
+
+    def test_correct_show_context_posts(self):
+        """ Проверка корректности контекста post. """
+
+        for url in self.urls_for_check_post_context:
+            response = self.authorized_client.get(url)
+
+            with self.subTest(url=url):
+                self.assertEqual(
+                    response.context.get('post'),
+                    TestingViews.post
+                )
+
+    def test_correct_show_context_is_edits(self):
+        """ Проверка корректности контекста is_edit. """
+
+        for url, value in self.urls_and_values_for_check_is_edit_context.items():
+            response = self.authorized_client.get(url)
+
+            with self.subTest(url=url):
+                self.assertEqual(
+                    response.context.get('is_edit'),
+                    value
+                )
+
+    def test_correct_show_context_group(self):
+        """ Проверка корректности контекста group, на странице групп. """
+        response_group = self.authorized_client.get(self.urls['group_posts'])
+
+        self.assertEqual(
+            TestingViews.group,
+            response_group.context.get('group')
+        )
+
+    def test_correct_show_context_is_author_if_user_authorized(self):
+        """ 
+        Проверка корректности контекста author,
+        если пользователь автор поста.
+        """
+
+        # Пользователь автор поста
+        response_author = self.authorized_client.get(self.urls['post_detail'])
+
+        self.assertEqual(
+            response_author.context.get('author'),
+            TestingViews.user
+        )
+
+    def test_correct_show_context_is_author_if_user_not_authorized(self):
+        """
+        Проверка корректности контекста author,
+        если пользователь не автор поста.
+        """
+
+        # Пользователь не автор поста
+        response_other = self.authorized_client_other.get(
+            self.urls['post_detail']
+        )
+
+        self.assertNotEqual(
+            response_other.context.get('author'),
+            TestingViews.user
+        )
+
+    def test_correct_show_context_posts_count(self):
+        """ Проверка корректности контекста количество постов. """
+
+        for url in self.urls_for_check_post_count_context:
+            response = self.authorized_client.get(url)
+
+            with self.subTest(url=url):
+                self.assertEqual(
+                    response.context.get('post_count'),
+                    Post.objects.count()
+                )
+
+    def test_correct_show_context_post_authors(self):
+        """ Проверка корректности контекста в авторах поста. """
+
+        for url in self.urls_for_check_author_context:
+            response = self.authorized_client.get(url)
+
+            with self.subTest(url=url):
+                self.assertEqual(
+                    response.context.get('author'),
+                    TestingViews.post.author
+                )
+
+    def test_correct_show_context_groups(self):
+        """ Проверка корректности контекста в группах. """
+
+        for url in self.urls_for_check_group_context:
+            response = self.authorized_client.get(url)
+
+            with self.subTest(url=url):
+                self.assertEqual(
+                    response.context.get('group'),
+                    TestingViews.post.group
+                )
+
+    def test_correct_show_context_authors(self):
+        """ Проверка корректности контекста авторов. """
+
+        for url in self.urls_for_check_authors_context:
+            response = self.authorized_client.get(url)
+
+            with self.subTest(url=url):
+                self.assertEqual(
+                    response.context.get('authors')[0],
+                    TestingViews.follow.author
+                )
+
+
+class TestingViewsCheckPagination(TestingViews):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.urls_pagination = (
+            cls.urls['index'],
+            cls.urls['group_posts'],
+            cls.urls['profile'],
+            cls.urls['follow_index'],
+            cls.urls['follow_author']
+        )
+
+        cls.GENERAL_AMOUNT_POSTS = 18
+
+        cls.AMOUNT_POSTS_ON_FIRST_PAGE = 10
+
+        cls.AMOUNT_POSTS_ON_SECOND_PAGE = 8
+
     def test_correct_show_pagination(self):
         """ Проверка корректности работы пагинатор. """
         old_posts = Post.objects.all()
         old_posts.delete()
 
-        AMOUNT_POSTS = 18
-
-        FIRST_TEN_POSTS = 10
-
-        SECOND_EIGHT_POSTS = 8
-
         # Создать количество постов
-        for i in range(AMOUNT_POSTS):
+        for i in range(self.GENERAL_AMOUNT_POSTS):
             Post.objects.create(
                 text='Описание поста',
-                author=TestingViewsInPosts.user,
-                group=TestingViewsInPosts.group
+                author=TestingViews.user,
+                group=TestingViews.group
             )
 
         for url in self.urls_pagination:
@@ -347,7 +340,7 @@ class TestingViewsInPosts(TestCase):
             with self.subTest(url=url):
                 self.assertEqual(
                     len(response_page_one.context.get('page_obj').object_list),
-                    FIRST_TEN_POSTS
+                    self.AMOUNT_POSTS_ON_FIRST_PAGE
                 )
 
             response_page_two = self.authorized_client.get(url + '?page=2')
@@ -356,9 +349,11 @@ class TestingViewsInPosts(TestCase):
             with self.subTest(url=url):
                 self.assertEqual(
                     len(response_page_two.context.get('page_obj').object_list),
-                    SECOND_EIGHT_POSTS
+                    self.AMOUNT_POSTS_ON_SECOND_PAGE
                 )
 
+
+class TestingViewsCheckСache(TestingViews):
     def test_check_cache_template(self):
         """ Проверка кэша в шаблоне. """
 
@@ -367,8 +362,8 @@ class TestingViewsInPosts(TestCase):
 
         post_cash = Post.objects.create(
             text='Тестирование кэша',
-            author=TestingViewsInPosts.user,
-            group=TestingViewsInPosts.group
+            author=TestingViews.user,
+            group=TestingViews.group
         )
 
         response_one = self.authorized_client.get(reverse('posts:index'))
@@ -390,3 +385,35 @@ class TestingViewsInPosts(TestCase):
             response_one.context.get('page_obj').object_list,
             response_three.context.get('page_obj').object_list
         )
+
+
+class TestingViewsCheckForm(TestingViews):
+    def test_correct_form_when_creating_posts(self):
+        """ Тестируем форму при создания поста. """
+        response = self.authorized_client.get(self.urls['post_create'])
+
+        form_fields = {
+            'text': forms.fields.CharField,
+            'group': forms.fields.ChoiceField
+        }
+
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_field = response.context['form'].fields[value]
+
+                self.assertIsInstance(form_field, expected)
+
+    def test_correct_form_when_editing_posts(self):
+        """ Тестируем форму при редактирования поста. """
+        response = self.authorized_client.get(self.urls['post_edit'])
+
+        form_fields = {
+            'text': forms.fields.CharField,
+            'group': forms.fields.ChoiceField
+        }
+
+        for value, expected in form_fields.items():
+            with self.subTest(value=value):
+                form_field = response.context['form'].fields[value]
+
+                self.assertIsInstance(form_field, expected)
